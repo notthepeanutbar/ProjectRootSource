9716885463468fa5e1f6cf38e09f5f19e2c7d8fa
notthepeanutbar <changagencyth@gmail.com>
2024-12-18 22:55:52 +0700

    Add code to framework_start.py: Initialize framework with execution and rabbit hole action.

--- code/framework_start.py
+++ code/framework_start.py
@@ -1,1 +1,19 @@
+# framework_start.py
 
+framework_exists = False  # Start status: No framework
+
+def build_framework():
+    global framework_exists
+    print("Building Framework...")
+    if execute_framework():
+        framework_exists = True
+        rabbit_hole()
+    else:
+        raise Exception("FrameworkExecutionError")
+
+def execute_framework():
+    print("Framework EXECUTE successful.")
+    return True
+
+def rabbit_hole():
+    print("ACTION: Go down the rabbit hole.")
